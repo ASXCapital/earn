54eabf460e0ff2dcea202dc3943f8f56b4b6d077
Tom_D3Fi <106840127+tomdef1@users.noreply.github.com>
2024-04-19 00:26:19 +0100

    css button fix

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -91,108 +91,17 @@ const Header = () => {
 
   */}
         </div>
-        <ConnectButton.Custom>
-          {({
-            account,
-            chain,
-            openAccountModal,
-            openChainModal,
-            openConnectModal,
-            authenticationStatus,
-            mounted,
-          }) => {
-            const ready = mounted && authenticationStatus !== "loading";
-            const connected =
-              ready &&
-              account &&
-              chain &&
-              (!authenticationStatus ||
-                authenticationStatus === "authenticated");
+        <ConnectButton
+          accountStatus={{
+            smallScreen: 'avatar',
+            largeScreen: 'full',
 
-            return (
-              <div
-                {...(!ready && {
-                  "aria-hidden": true,
-                  style: {
-                    opacity: 0,
-                    pointerEvents: "none",
-                    userSelect: "none",
-                  },
-                })}
-              >
-                {(() => {
-                  if (!connected) {
-                    return (
-                      <button
-                        className={styles.buttonStyle}
-                        onClick={openConnectModal}
-                        type="button"
-                      >
-                        Connect Wallet
-                      </button>
-                    );
-                  }
-
-                  if (chain.unsupported) {
-                    return (
-                      <button
-                        className={styles.buttonStyle}
-                        onClick={openChainModal}
-                        type="button"
-                      >
-                        Wrong network
-                      </button>
-                    );
-                  }
-
-                  return (
-                    <div style={{ display: "flex", gap: 12 }}>
-                      <button
-                        className={styles.buttonStyle}
-                        onClick={openChainModal}
-                        style={{ display: "flex", alignItems: "center" }}
-                        type="button"
-                      >
-                        {chain.hasIcon && (
-                          <div
-                            style={{
-                              background: chain.iconBackground,
-                              width: 12,
-                              height: 12,
-                              borderRadius: 999,
-                              overflow: "hidden",
-                              marginRight: 4,
-                            }}
-                          >
-                            {chain.iconUrl && (
-                              <img
-                                alt={chain.name ?? "Chain icon"}
-                                src={chain.iconUrl}
-                                style={{ width: 12, height: 12 }}
-                              />
-                            )}
-                          </div>
-                        )}
-                        {chain.name}
-                      </button>
-
-                      <button
-                        className={styles.buttonStyle}
-                        onClick={openAccountModal}
-                        type="button"
-                      >
-                        {account.displayName}
-                        {account.displayBalance
-                          ? ` (${account.displayBalance})`
-                          : ""}
-                      </button>
-                    </div>
-                  );
-                })()}
-              </div>
-            );
           }}
-        </ConnectButton.Custom>
+          showBalance={{
+            smallScreen: false,
+            largeScreen: true,
+          }}
+        />
       </nav>
     </header>
   );
